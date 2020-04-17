2f5afb99bc8495250e743e8829a1ae4472e980d9
jevvilla <juan.es-19@hotmail.com>
2020-04-17 18:15:44 -0500

    Home and Details screen created, screen barrel file was created also to wrap all screen file exports

---
+++ src/screens/details/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import styles from './styles';
+
+export const Details = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Details</Text>
+    </View>
+  );
+};
