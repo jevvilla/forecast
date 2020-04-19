744f9cbcdb6fc476d84074196b465d134df063f2
jevvilla <juan.es-19@hotmail.com>
2020-04-19 14:41:24 -0500

    types file updated, Prediction type was added to be used in the feed data comming from the api request

--- src/common/types.ts
+++ src/common/types.ts
@@ -1,3 +1,57 @@
+type Coordinates = {
+  lon: number;
+  lat: number;
+};
+
+type Weather = {
+  id: number;
+  main: string;
+  description: string;
+  icon: string;
+};
+
+type Main = {
+  temp: number;
+  feels_like: number;
+  temp_min: number;
+  temp_max: number;
+  pressure: number;
+  humidity: number;
+};
+
+type Wind = {
+  speed: number;
+  deg: number;
+};
+
+type Clouds = {
+  all: number;
+};
+
+type Sys = {
+  type: number;
+  id: number;
+  country: string;
+  sunrise: number;
+  sunset: number;
+};
+
+export type Prediction = {
+  coordinates: Coordinates;
+  weather: Weather[];
+  base: string;
+  main: Main;
+  visibility: number;
+  wind: Wind;
+  clouds: Clouds;
+  dt: number;
+  sys: Sys;
+  timezone: number;
+  id: number;
+  name: string;
+  cod: number;
+};
+
 export type HomeScreenParamList = {
   Details: Object;
 };
