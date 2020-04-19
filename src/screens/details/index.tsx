d0abe46405874b45218b63097239466881ad7daf
jevvilla <juan.es-19@hotmail.com>
2020-04-19 15:17:40 -0500

    Details screen info rendered and request api made to get info

--- src/screens/details/index.tsx
+++ src/screens/details/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, ActivityIndicator } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
+import FeatherIcon from 'react-native-vector-icons/Feather';
 
-import { DetailsScreenParamList } from '../../common/types';
+import { DetailsScreenParamList, Prediction } from '../../common/types';
+import { getTimeFromTimestamp } from '../../common/utils';
 import * as routes from '../../navigation/routes';
+import { getPredictions } from '../../api';
 
 import styles from './styles';
 
@@ -12,9 +15,63 @@ type Props = {
 };
 
 export const Details: React.FC<Props> = ({ navigation }) => {
+  const [prediction, setPrediction] = React.useState<Prediction>();
+
+  React.useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const data = await getPredictions();
+      setPrediction(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  if (!prediction) {
+    return <ActivityIndicator size="small" color="rgb(63,138,247)" />;
+  }
   return (
     <View style={styles.container}>
-      <Text>Details</Text>
+      <View style={styles.main}>
+        <Text style={styles.title}>{Math.round(prediction.main.temp)}º</Text>
+        <FeatherIcon name="sun" size={110} color="rgb(236, 177, 63)" />
+      </View>
+      <View style={styles.mainTextContainer}>
+        <View style={styles.minmax}>
+          <Text style={[styles.mainLabel, styles.boldLabel]}>{prediction.weather[0].main} day</Text>
+          <Text style={styles.mainLabel}>
+            {Math.round(prediction.main.temp_max)}º / {Math.round(prediction.main.temp_min)}º
+          </Text>
+        </View>
+        <View>
+          <Text style={styles.mainLabel}>Feels like {Math.round(prediction.main.feels_like)}º</Text>
+        </View>
+      </View>
+      <View style={styles.infoContainer}>
+        <View style={styles.infoBox}>
+          <FeatherIcon name="sunrise" size={35} color="rgb(63,138,247)" />
+          <Text style={styles.infoLabel}>{getTimeFromTimestamp(prediction.sys.sunrise)}</Text>
+        </View>
+        <View style={styles.infoBox}>
+          <FeatherIcon name="sunset" size={35} color="rgb(63,138,247)" />
+          <Text style={styles.infoLabel}>{getTimeFromTimestamp(prediction.sys.sunset)}</Text>
+        </View>
+        <View style={styles.infoBox}>
+          <FeatherIcon name="wind" size={35} color="rgb(63,138,247)" />
+          <Text style={styles.infoLabel}>{prediction.wind.speed} km/h</Text>
+        </View>
+        <View style={styles.infoBox}>
+          <FeatherIcon name="cloud" size={35} color="rgb(63,138,247)" />
+          <Text style={styles.infoLabel}>{prediction.clouds.all}%</Text>
+        </View>
+        <View style={styles.infoBox}>
+          <FeatherIcon name="umbrella" size={35} color="rgb(63,138,247)" />
+          <Text style={styles.infoLabel}>{prediction.main.humidity}%</Text>
+        </View>
+      </View>
       <Button onPress={() => navigation.push(routes.HOME)} title="go to Home" />
     </View>
   );
--- src/screens/details/styles.ts
+++ src/screens/details/styles.ts
@@ -3,7 +3,47 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    padding: 16,
+  },
+  main: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 10,
+  },
+  mainTextContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingRight: 8,
+  },
+  minmax: {
+    flexDirection: 'row',
+  },
+  boldLabel: {
+    fontFamily: 'Lato-Bold',
+  },
+  mainLabel: {
+    fontFamily: 'Lato-Regular',
+    fontSize: 16,
+    paddingHorizontal: 4,
+  },
+  title: {
+    fontFamily: 'Lato-Bold',
+    fontSize: 100,
+  },
+  infoContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginVertical: 30,
+  },
+  infoBox: {
+    flexDirection: 'row',
+    flexBasis: '33.3%',
     alignItems: 'center',
+    height: 50,
+  },
+  infoLabel: {
+    marginLeft: 14,
+    fontFamily: 'Lato-Bold',
+    fontSize: 18,
   },
 });
