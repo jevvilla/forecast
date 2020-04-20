5e2222de44e0902e2d9127b77cc309360612542b
jevvilla <juan.es-19@hotmail.com>
2020-04-20 16:21:10 -0500

    shadow style was created in common style file

---
+++ src/common/styles.ts
@@ -0,0 +1,12 @@
+export const shadow = () => {
+  return {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.16,
+    shadowRadius: 2,
+    elevation: 4,
+  };
+};
